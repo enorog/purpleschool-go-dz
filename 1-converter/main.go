5e4777ae95266c71ecc7eb3d3387495ec4e2340f
enorog <231403899+enorog@users.noreply.github.com>
2025-10-24 17:36:34 +0300

    Создал заготовку для метода конвертации валют

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func getConverterInput() (sourceAmount float64, sourceCurrency string, targetCurrency string) {
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scanf("%3s\n", &sourceCurrency)
+	fmt.Print("Введите сумму ковертации: ")
+	fmt.Scanf("%f\n", &sourceAmount)
+	fmt.Printf("Веедите целевую валюту: ")
+	fmt.Scanf("%3s\n", &targetCurrency)
+	return
+}
+
+func convertCurrency(sourceamount float64, sourceCurrency string, targetCurrency string) float64 {
+	return 0
+}
+
 func main() {
 	const USD_TO_EUR = 0.8
 	const USD_TO_RUB = 80.2
@@ -11,4 +25,9 @@ func main() {
 	fmt.Printf("USD_TO_EUR: %f\n", USD_TO_EUR)
 	fmt.Printf("USD_TO_RUB: %f\n", USD_TO_RUB)
 	fmt.Printf("EUR_TO_RUB: %f\n", EUR_TO_RUB)
+
+	sourceamount, sourceCurrency, targetCurrency := getConverterInput()
+
+	convertedAmount := convertCurrency(sourceamount, sourceCurrency, targetCurrency)
+	fmt.Printf("Сумма в целевой валюте: %.2f\n", convertedAmount)
 }
