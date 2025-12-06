ff2c935ad70d2bfcfbf0f676429ad3888b04b09d
enorog <231403899+enorog@users.noreply.github.com>
2025-12-06 21:59:20 +0300

    Добавил создание конфига

---
+++ 4-cli/api/api.go
@@ -0,0 +1,7 @@
+package api
+
+import "cli/config"
+
+func UseConfig(config *config.Config) {
+
+}
--- 4-cli/main.go
+++ 4-cli/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"cli/api"
+	"cli/config"
 	"cli/files"
 	"cli/storage"
 	"fmt"
@@ -10,6 +12,12 @@ import (
 const storagePath = "./storage.json"
 
 func main() {
+	config, err := config.NewConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка получения конфига: %v\n", err)
+		os.Exit(1)
+	}
+	api.UseConfig(config)
 	fileProvider, err := files.NewJsonFile(storagePath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Ошибка инициализации провайдера: %v", err)
